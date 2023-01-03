50c5f0ea281d1f4d13a60de02ce6c35902b517ba
jitain1 <singhjitainj@gmail.com>
2023-01-03 21:18:54 +0530

    Checked equality between two lines

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,5 +1,7 @@
 package com.bridgelabz;
 
+import java.util.Objects;
+
 public class LineComparison {
 	static int x1;
 	static int y1;
@@ -19,6 +21,19 @@ public class LineComparison {
 
         int length1 = (int) Math.sqrt( (Math.pow((x1-x2),2) + Math.pow((y1-y2),2)) );
         System.out.println("Length of the line : " + length1);
+        x1 = 3;
+		y1 = 6;
+		
+		x2 = 2;
+		y2 = -5;
+		
+		int length2 = (int) Math.sqrt( (Math.pow((x1-x2),2) + Math.pow((y1-y2),2)) );
+		System.out.println("Length of the line : " + length2);
+		
+		if(Objects.equals(length1,length2)) {
+			System.out.println("Both Length are same");
+		}else
+			System.out.println("Both Length are not same");
 
 
 	}
