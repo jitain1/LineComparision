d28014eed7af10c24302fbbb821a9e2bea426ff8
jitain1 <singhjitainj@gmail.com>
2023-01-03 21:01:50 +0530

    Two lengths are added

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,10 +1,25 @@
 package com.bridgelabz;
 
 public class LineComparison {
+	static int x1;
+	static int y1;
+
+    static int x2;
+    static int y2;
+
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to line comparision");
+        x1 = 2;
+        y1 = -5;
+
+        x2 = 3;
+        y2 = 6;
+
+        int length1 = (int) Math.sqrt( (Math.pow((x1-x2),2) + Math.pow((y1-y2),2)) );
+        System.out.println("Length of the line : " + length1);
+
 
 	}
 
